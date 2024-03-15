03d0761a7ae97be880d9a1991d7250cad8c96bea
Antoniongola <Antoniongola10@gmail.com>
2024-03-15 20:25:59 +0100

    Primeiro commit

---
+++ UsersControlApi/src/main/java/com/ngolajr/userscontrol/UsersControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.ngolajr.userscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersControlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersControlApiApplication.class, args);
+    }
+
+}
